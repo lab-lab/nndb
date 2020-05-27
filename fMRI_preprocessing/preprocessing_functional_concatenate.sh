7e4af2a22750fe4c7e677e6fbeae22ae2bbcc66a
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-05-27 17:49:40 +0100

    Update preprocessing_functional_concatenate.sh

--- fMRI_preprocessing/preprocessing_functional_concatenate.sh
+++ fMRI_preprocessing/preprocessing_functional_concatenate.sh
@@ -6,46 +6,45 @@
 
 # NOTE
 # Used for, e.g., ICA, 3dDegreeCentrality, etc.
-# Will not work for more than 9 functional files
 
-for perp in $perps
+for subject in $participants
 do
-  # Change to perp directory
-  cd "$data_dir"/"$perp"/
+  # Change to subject directory
+  cd "$subject"/
   for filename in norm polort norm_polort norm_polort_motion norm_polort_motion_wm_ventricle
   do
     # mv files back if necessary
-    mv ./files/timeseries_files/media_?_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz ./
-    mv ./files/timeseries_files/media_?_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz ./
+    mv ./files/timeseries_files/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz ./
+    mv ./files/timeseries_files/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz ./
     # Concatenate non-blur
     3dTcat \
-      -prefix media_all_tshift_despike_reg_al_mni_mask_"$filename"_timing_long.nii.gz \
-              media_?_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz
+      -prefix ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing_long.nii.gz \
+              ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz
     # Remove old non-blur results
-    rm media_all_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz
-    rm ./files/timeseries_files/media_all_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz
+    rm ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz
+    rm ./files/timeseries_files/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz
     # Set afni tp length
     movie_length_1=`echo $((movie_length - 1))`
     # Make length
     3dTcat \
-      -prefix media_all_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz \
-              media_all_tshift_despike_reg_al_mni_mask_"$filename"_timing_long.nii.gz[0.."$movie_length_1"]
+      -prefix ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing.nii.gz \
+              ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing_long.nii.gz[0.."$movie_length_1"]
     # Remove non-blur long file
-    rm media_all_tshift_despike_reg_al_mni_mask_"$filename"_timing_long.nii.gz
+    rm ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_"$filename"_timing_long.nii.gz
     # Concatenate blur
     3dTcat \
-      -prefix media_all_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing_long.nii.gz \
-              media_?_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz
+      -prefix ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing_long.nii.gz \
+              ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz
     # Remove old blur results
-    rm media_all_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz
-    rm ./files/timeseries_files/media_all_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz
+    rm ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz
+    rm ./files/timeseries_files/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz
     # Set afni tp length
     movie_length_1=`echo $((movie_length - 1))`
     3dTcat \
-      -prefix media_all_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz \
-              media_all_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing_long.nii.gz[0.."$movie_length_1"]
+      -prefix ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing.nii.gz \
+              ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing_long.nii.gz[0.."$movie_length_1"]
     # Remove blur long file
-    rm media_all_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing_long.nii.gz
+    rm ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_al_mni_mask_blur"$functional_blur_amount"_"$filename"_timing_long.nii.gz
   done
 done
 
