ddf6da67a4c50489d6fdd5bc37b9e8dbce4ec78c
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-05-27 17:45:45 +0100

    Update preprocessing_functional_align_to_mni_anatomical.sh

--- fMRI_preprocessing/preprocessing_functional_align_to_mni_anatomical.sh
+++ fMRI_preprocessing/preprocessing_functional_align_to_mni_anatomical.sh
@@ -4,25 +4,22 @@
 # Align unbiased functional data to the MNI aligned anatomical image
 ################################################################################
 
-# NOTE
-# This step might be better done later in the game
-
-for perp in $perps
+for subject in $participants
 do
-    cd "$data_dir"/"$perp"/
-    total_num_runs=`ls ./media_?.nii.gz  | wc -l`
+    cd "$subject"/
+    total_num_runs=`ls ./func/"$subject"_task-"$movie"_run-0?_bold.nii.gz  | wc -l`
     for run in `seq 1 $total_num_runs`
     do
-        rm media_"$run"_tshift_despike_reg_al.nii.gz
+        rm ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg_al.nii.gz
         3dcopy \
-            media_"$run"_tshift_despike_reg_al+orig \
-            media_"$run"_tshift_despike_reg_al.nii.gz
-        rm media_"$run"_tshift_despike_reg_al_mni.nii.gz
+            ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg_al+orig \
+            ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg_al.nii.gz
+        rm ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg_al_mni.nii.gz
         3dNwarpApply \
-            -master anatomical_avg_uni_ns.aw.nii.gz -dxyz 3 \
-            -source media_"$run"_tshift_despike_reg_al.nii.gz \
+            -master ./anat/"$subject"_T1w_avg_uni_ns.aw.nii.gz -dxyz 3 \
+            -source ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg_al.nii.gz \
             -nwarp 'awpy/anat.un.aff.qw_WARP.nii.gz awpy/anat.un.aff.Xat.1D' \
-            -prefix media_"$run"_tshift_despike_reg_al_mni.nii.gz
+            -prefix ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg_al_mni.nii.gz
     done
 done
 
