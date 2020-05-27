72211ce9dd0fa4c138488ce1ddc0d579c9410102
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-05-27 17:43:45 +0100

    Update preprocessing_functional_align_to_aligned_anatomical.sh

--- fMRI_preprocessing/preprocessing_functional_align_to_aligned_anatomical.sh
+++ fMRI_preprocessing/preprocessing_functional_align_to_aligned_anatomical.sh
@@ -8,22 +8,22 @@
 # This isn't the same as the previously made alignment image
 # This one is made after functional vilume registration so should be slightly better
 
-for perp in $perps
+for subject in $participants
 do
 
-    cd "$data_dir"/"$perp"/
+    cd "$subject"/
 
     # Set centre run again
-    num_files=`ls media_?.nii.gz | wc -l`
+    num_files=`ls ./func/"$subject"_task-"$movie"_run-0?_bold.nii.gz | wc -l`
     set_center_run=`echo "$((($num_files + 1) / 2))"`
 
     # Remove any old files
-    rm media_tshift_despike_reg_align_image.nii.gz
+    rm ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_align_image.nii.gz
     3dTstat \
         -datum float \
         -mean \
-        -prefix media_tshift_despike_reg_align_image.nii.gz \
-                media_"$set_center_run"_tshift_despike_reg.nii.gz
+        -prefix ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_align_image.nii.gz \
+                ./func/"$subject"_task-"$movie"_run-0"$set_center_run"_bold_tshift_despike_reg.nii.gz
 
 ################################################################################
 # Align the functional data to aligned anatomical image
@@ -31,31 +31,31 @@ do
 
 # NOTE
 # This step comes before alignment to the MNI brain in order to deoblique
-# This is done after despikeing b/c, again, the help file states:
+# This is done after despiking b/c the help file states:
 # 'it seems like the registration problem does NOT happen, and in fact, despiking seems to help!'
 # -big_move doesn't work
 
     # Remove any lingering files from prior attempts
-    rm media_?_*_al*
-    rm media_tshift_despike_reg_align_image_al+orig.*
+    rm ./func/"$subject"_task-"$movie"_run-0?_bold_*_al*
+    rm ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_align_image_al+orig.*
     rm __tt_*+orig.*
-    rm __tt_anatomical_avg_uni_ns_obla2e_mat.1D
-    rm anatomical_avg_uni_ns_al_e2a_only_mat.aff12.1D
+    rm __tt_"$subject"_T1w_avg_uni_ns_obla2e_mat.1D
+    rm "$subject"_T1w_avg_uni_ns_al_e2a_only_mat.aff12.1D
 
     # Move in files if needed
-    mv files/anatomical_files/anatomical_avg_uni_ns.nii.gz ./
+    mv files/anatomical_files/"$subject"_T1w_avg_uni_ns.nii.gz ./
 
     # Align
     align_epi_anat.py \
         -epi2anat -giant_move -partial_coverage -deoblique on -anat_has_skull no -tshift off -volreg off -epi_base 0 \
-        -anat anatomical_avg_uni_ns.nii.gz \
-        -epi  media_tshift_despike_reg_align_image.nii.gz \
+        -anat ./anat/"$subject"_T1w_avg_uni_ns.nii.gz \
+        -epi  ./func/"$subject"_task-"$movie"_bold_reg_align_image.nii.gz \
         -child_epi \
-         media_?_tshift_despike_reg.nii.gz
+         ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg.nii.gz
 
     # Remove __* files (not sure why they hang around)
     rm __tt_*+orig.*
-    rm __tt_anatomical_avg_uni_ns_obla2e_mat.1D
+    rm __tt_"$subject"_T1w_avg_uni_ns_obla2e_mat.1D
 
     # Compress BRIK files
     bzip2 *BRIK
