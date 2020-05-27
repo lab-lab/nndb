67cfe7a5ba8f8b75273ef64ce1f178875bdc484a
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-05-27 17:34:40 +0100

    Update preprocessing_anatomical_skull_strip.sh

--- fMRI_preprocessing/preprocessing_anatomical_skull_strip.sh
+++ fMRI_preprocessing/preprocessing_anatomical_skull_strip.sh
@@ -5,28 +5,29 @@
 ################################################################################
 
 # NOTE:
-# I think ROBEX does a better job skull stripping than the programs for doing so in AFNI (3dSkullStrip) or freesurfer or FSL
-# Test (stripping and alignment) for blurred/uniformized vs not - seems to be better for the latter?
+# ROBEX does a better job at skull stripping than the programs for doing so in AFNI (3dSkullStrip) or freesurfer or FSL
+# Set RROBEX directory path!
+robex_dir=""
 
-for perp in $perps
+for subject in $participants
 do
 
-  cd "$data_dir"/"$perp"/
+  cd "$subject"/
 
-  /usr/local/ROBEX/runROBEX.sh \
-    anatomical_avg_uni.nii.gz \
-    anatomical_avg_uni_ns.nii.gz
+  "$robex_dir"/runROBEX.sh \
+    ./anat/"$subject"_T1w_avg_uni.nii.gz \
+    ./anat/"$subject"_T1w_avg_uni_ns.nii.gz
 
-  /usr/local/ROBEX/runROBEX.sh \
-    anatomical_avg.nii.gz \
-    anatomical_avg_ns.nii.gz
+  "$robex_dir"/runROBEX.sh \
+    ./anat/"$subject"_T1w_avg.nii.gz \
+    ./anat/"$subject"_T1w_avg_ns.nii.gz
 
 # NOTE
-# anatomical_avg_uni.nii.gz IS the blurred version (see prior step)
+# ./anat/"$subject"_T1w_avg_uni.nii.gz IS the blurred version (see prior step)
 # blur_amount=1
-# /usr/local/ROBEX/runROBEX.sh \
-# "$data_dir"/"$perp_dir"/anatomical_1_blur"$blur_amount"_uni.nii.gz \
-# "$data_dir"/"$perp_dir"/anatomical_avg_uni_ns.nii.gz
+# "$roobex_dir"/runROBEX.sh \
+# "$subject"/anat/"$subject"_T1w_blur"$blur_amount"_uni.nii.gz \
+# "$subject"/anat/"$subject"_T1w_avg_uni_ns.nii.gz
 
 done
 
