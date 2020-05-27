e7f9b1d6414485c2bf3322ea6e9d2afa41e88d80
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-05-27 19:07:20 +0100

    Update preprocessing_general_move_files.sh

--- fMRI_preprocessing/preprocessing_general_move_files.sh
+++ fMRI_preprocessing/preprocessing_general_move_files.sh
@@ -4,45 +4,39 @@
 # Move files out of the way
 ################################################################################
 
-# NOTE
-# Used "?". Would need ?? if you have more than 9 files
-
 for perp in $perps
 do
-    cd "$data_dir"/"$perp"/
-    mv anatomical_1* \
-       anatomical_2* \
-       anatomical_avg.nii.gz \
-       anatomical_avg_uni.nii.gz \
-       anatomical_avg_uni_ns.nii.gz \
-       anatomical_avg_ns.nii.gz \
-       anatomical_avg_uni.aw.nii.gz \
-       anatomical_avg_uni_ns.aw_mask.nii.gz \
-       anatomical_avg_uni_ns.aw_mask_resample.nii.gz \
+    cd "$subject"/
+    mv ./anat/"$subject"_T1w_avg.nii.gz \
+       ./anat/"$subject"_T1w_avg_uni.nii.gz \
+       ./anat/"$subject"_T1w_avg_uni_ns.nii.gz \
+       ./anat/"$subject"_T1w_avg_ns.nii.gz \
+       ./anat/"$subject"_T1w_avg_uni.aw.nii.gz \
+       ./anat/"$subject"_T1w_avg_uni_ns.aw_mask.nii.gz \
+       ./anat/"$subject"_T1w_avg_uni_ns.aw_mask_resample.nii.gz \
        files/anatomical_files/
-    mv media_?.nii.gz \
-       media_?_?_tshift_* \
-       *_interpolation_amount.nii.gz \
-       *_tp.nii.gz \
-       *_slug.nii.gz \
-       media_?_tshift.nii.gz \
-       media_tshift_despike_align_image.nii.gz \
-       media_?_tshift_despike.nii.gz \
-       media_?_tshift_despike_reg.nii.gz \
-       media_tshift_despike_reg_align_image.nii.gz \
-       media_tshift_despike_reg_align_image_al+* \
-       media_?_tshift_despike_reg_al+orig* \
-       media_?_tshift_despike_reg_al.nii.gz \
-       media_?_tshift_despike_reg_al_mni.nii.gz \
-       media_?_tshift_despike_reg_al_mni_mask.nii.gz \
-       media_?_tshift_despike_reg_al_mni_mask_*.nii.gz \
+       
+    mv ./func/"$subject"_task-"$movie"_run-0?_run-0?_bold_tshift_* \
+       ./func/*_interpolation_amount.nii.gz \
+       ./func/*_tp.nii.gz \
+       ./func/*_slug.nii.gz \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift.nii.gz \
+       ./func/"$subject"_task-"$movie"_bold_tshift_despike_align_image.nii.gz \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike.nii.gz \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg.nii.gz \
+       ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_align_image.nii.gz \
+       ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_align_image_al+* \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al+orig* \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al.nii.gz \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al_mni.nii.gz \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al_mni_mask.nii.gz \
+       ./func/"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_al_mni_mask_*.nii.gz \
        files/timeseries_files/
-    mv media_?_tshift_despike_reg_*_mat.aff12.1D \
-       anatomical_avg_uni_ns_al_e2a_only_mat.aff12.1D \
-       files/anatomical_files/anatomical_2_blur1_uni_al_mat.aff12.1D \
-       media_tshift_despike_reg_align_image_al_mat.aff12.1D \
+    mv ./"$subject"_task-"$movie"_run-0?_bold_tshift_despike_reg_*_mat.aff12.1D \
+       ./"$subject"_T1w_avg_uni_ns_al_e2a_only_mat.aff12.1D \
+       ./func/"$subject"_task-"$movie"_bold_tshift_despike_reg_align_image_al_mat.aff12.1D \
        files/text_files/
-    mv media_*deoblique* \
+    mv ./func/"$subject"_task-"$movie"_*deoblique* \
        files/timeseries_files/
     mv ventricle_mask.nii.gz \
        ventricle_mask_resample_erode.nii.gz \
