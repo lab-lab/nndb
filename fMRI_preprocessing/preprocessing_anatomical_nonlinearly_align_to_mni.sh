2c341557324df6fdfdf37dd1b082a88889f8cd88
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-05-27 17:31:46 +0100

    Update preprocessing_anatomical_nonlinearly_align_to_mni.sh

--- fMRI_preprocessing/preprocessing_anatomical_nonlinearly_align_to_mni.sh
+++ fMRI_preprocessing/preprocessing_anatomical_nonlinearly_align_to_mni.sh
@@ -4,40 +4,40 @@
 # Nonlinearly align grand averaged anatomical image to MNI template
 ################################################################################
 
-# NOTE:
-# suma -spec /usr/local/suma_MNI_N27/MNI_N27_both.spec -sv anatomical_avg_uni_ns_al.aw.nii.gz
+# Make sure you set the suma directory path!
+suma_dir=""
 
-for perp in $perps
+for subject in $participants
 do
 
-  cd "$data_dir"/"$perp"/
+  cd "$subject"/
 
   auto_warp.py \
-    -base /usr/local/suma_MNI_N27/MNI_N27_SurfVol.nii \
-    -input anatomical_avg_uni_ns.nii.gz \
+    -base "$suma_dir"/MNI_N27_SurfVol.nii \
+    -input ./anat/"$subject"_T1w_avg_uni_ns.nii.gz \
     -skull_strip_base no -skull_strip_input no
 
   # Compress files created during the alignment process
   gzip awpy/*nii
 
   # Move aligned brain into base directory
-  cp awpy/anatomical_avg_uni_ns.aw.nii.gz ./
+  cp awpy/"$subject"_T1w_avg_uni_ns.aw.nii.gz ./
 
   # Apply nonlinear alignment to skulled anatomical
-  rm anatomical_avg.aw.nii.gz
+  rm ./anat/"$subject"_T1w_avg.aw.nii.gz
   3dNwarpApply \
-    -master anatomical_avg_uni_ns.aw.nii.gz \
-    -source anatomical_avg.nii.gz \
-    -nwarp ''$data_dir'/'$perp'/awpy/anat.un.aff.qw_WARP.nii.gz '$data_dir'/'$perp'/awpy/anat.un.aff.Xat.1D' \
-    -prefix anatomical_avg.aw.nii.gz
+    -master ./anat/"$subject"_T1w_avg_uni_ns.aw.nii.gz \
+    -source ./anat/"$subject"_T1w_avg.nii.gz \
+    -nwarp ''$subject'/awpy/anat.un.aff.qw_WARP.nii.gz '$subject'/awpy/anat.un.aff.Xat.1D' \
+    -prefix ./anat/"$subject"_T1w_avg.aw.nii.gz
 
   # Apply nonlinear alignment to skulled uniformized anatomical
-  rm anatomical_avg_uni.aw.nii.gz
+  rm ./anat/"$subject"_T1w_avg_uni.aw.nii.gz
   3dNwarpApply \
-    -master anatomical_avg_uni_ns.aw.nii.gz \
-    -source anatomical_avg_uni.nii.gz \
-    -nwarp ''$data_dir'/'$perp'/awpy/anat.un.aff.qw_WARP.nii.gz '$data_dir'/'$perp'/awpy/anat.un.aff.Xat.1D' \
-    -prefix anatomical_avg_uni.aw.nii.gz
+    -master ./anat/"$subject"_T1w_avg_uni_ns.aw.nii.gz \
+    -source ./anat/"$subject"_T1w_avg_uni.nii.gz \
+    -nwarp ''$subject'/awpy/anat.un.aff.qw_WARP.nii.gz '$subject'/awpy/anat.un.aff.Xat.1D' \
+    -prefix ./anat/"$subject"_T1w_avg_uni.aw.nii.gz
 
 done
 
