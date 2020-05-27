996d90bda711879008b3ea7a2b1b758fe3db6dff
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-05-27 19:02:49 +0100

    Update preprocessing_functional_volume_registration.sh

--- fMRI_preprocessing/preprocessing_functional_volume_registration.sh
+++ fMRI_preprocessing/preprocessing_functional_volume_registration.sh
@@ -4,21 +4,19 @@
 # Create mean functional image for functional volume registration
 ################################################################################
 
-# NOTE
 
-for perp in $perps
+for subject in $participants
 do
-  cd "$data_dir"/"$perp"/
-  # set_center_run=1
-  rm media_tshift_despike_align_image.nii.gz
-  num_files=`ls media_?.nii.gz | wc -l`
+  cd "$subject"/
+  rm ./func/"$subject"_task-"$movie"_bold_tshift_despike_align_image.nii.gz
+  num_files=`ls ./func/"$subject"_task-"$movie"_run-0?_bold.nii.gz | wc -l`
   set_center_run=`echo "$((($num_files + 1) / 2))"`
   echo "* There are " $num_files "runs and the centre run will be " $set_center_run
   3dTstat \
       -datum float \
       -mean \
-      -prefix media_tshift_despike_align_image.nii.gz \
-              media_"$set_center_run"_tshift_despike.nii.gz
+      -prefix ./func/"$subject"_task-"$movie"_bold_tshift_despike_align_image.nii.gz \
+              ./func/"$subject"_task-"$movie"_run-0"$set_center_run"_bold_tshift_despike.nii.gz
 
 ################################################################################
 # Functional volume registration
@@ -28,18 +26,18 @@ do
 # This step aligns each whole brain collection to the alignment image created above
 # *** These files get demeaned later in the processing series for use in detrending
 
-  total_num_runs=`ls ./media_?.nii.gz  | wc -l`
+  total_num_runs=`ls ./func/"$subject"_task-"$movie"_run-0?_bold.nii.gz | wc -l`
   for run in `seq 1 $total_num_runs`
   do
-    rm media_"$run"_tshift_despike_reg.nii.gz
+    rm ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg.nii.gz
     3dvolreg \
         -float -twopass -twodup \
-        -1Dfile        files/text_files/media_"$run"_motion.1D \
-        -maxdisp1D     files/text_files/media_"$run"_maxdisp.1D \
-        -1Dmatrix_save files/text_files/media_"$run"_realign_mat.aff12.1D \
-        -base          media_tshift_despike_align_image.nii.gz \
-        -prefix        media_"$run"_tshift_despike_reg.nii.gz \
-                       media_"$run"_tshift_despike.nii.gz
+        -1Dfile        files/text_files/"$subject"_task-"$movie"_run-0"$run"_bold_motion.1D \
+        -maxdisp1D     files/text_files/"$subject"_task-"$movie"_run-0"$run"_bold_maxdisp.1D \
+        -1Dmatrix_save files/text_files/"$subject"_task-"$movie"_run-0"$run"_bold_realign_mat.aff12.1D \
+        -base          ./func/"$subject"_task-"$movie"_bold_tshift_despike_align_image.nii.gz \
+        -prefix        ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike_reg.nii.gz \
+                       ./func/"$subject"_task-"$movie"_run-0"$run"_bold_tshift_despike.nii.gz
   done
 
 ################################################################################
@@ -55,20 +53,20 @@ do
 
   for move_type in motion maxdisp
   do
-    rm files/text_files/media_"$move_type".1D
+    rm files/text_files/"$subject"_task-"$movie"_bold_"$move_type".1D
     cat \
-      files/text_files/media_1_"$move_type".1D  \
-      files/text_files/media_2_"$move_type".1D  \
-      files/text_files/media_3_"$move_type".1D  \
-      files/text_files/media_4_"$move_type".1D  \
-      files/text_files/media_5_"$move_type".1D  \
-      files/text_files/media_6_"$move_type".1D  \
-      files/text_files/media_7_"$move_type".1D  \
-      files/text_files/media_8_"$move_type".1D  \
-      files/text_files/media_9_"$move_type".1D  \
-      files/text_files/media_10_"$move_type".1D \
-      > files/text_files/media_"$move_type".1D
-      1dplot -pngs 2000 "$move_type"_plot files/text_files/media_"$move_type".1D
+      files/text_files/"$subject"_task-"$movie"_run-01_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-02_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-03_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-04_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-05_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-06_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-07_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-08_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-09_bold_"$move_type".1D  \
+      files/text_files/"$subject"_task-"$movie"_run-10_bold_"$move_type".1D \
+      > files/text_files/"$subject"_task-"$movie"_bold_"$move_type".1D
+      1dplot -pngs 2000 "$move_type"_plot files/text_files/"$subject"_task-"$movie"_bold_"$move_type".1D
     done
 done
 
